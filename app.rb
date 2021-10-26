8b1d4fdf7b9bc18db680596e9d26a989ddee5d6a
Caleb Anderson <buyananderson@gmail.com>
2021-10-26 15:17:30 +0200

    Create the app class

---
+++ app.rb
@@ -0,0 +1,29 @@
+class App
+    def initialize
+        @genres = []
+        @labels = []
+        @authors = []
+        @sources = []
+        @books = []
+        @musicAlbum = []
+        @movies = []
+        @games = []
+    end
+
+
+    def  self.add_genre(genre)
+        @genres<< genre        
+    end
+
+    def  self.add_genre(genre)
+        @genres<< genre        
+    end
+
+    def  self.add_genre(genre)
+        @genres<< genre        
+    end
+
+    def  self.add_genre(genre)
+        @genres<< genre        
+    end
+end
