e030ed22ff1ee8047f369f5dd459ee42e85ff630
Caleb Anderson <buyananderson@gmail.com>
2021-10-26 17:14:45 +0200

    Create the json method reader

--- file_manger.rb
+++ file_manger.rb
@@ -4,6 +4,10 @@ def createFile (file_name)
     FileUtils.touch(file_name)
 end
 
-def read_file(file_name)
+def save_file(file_name, content)
     File.write(file_name, JSON.dump(content))
+end
+
+def read_file(file_name)
+    JSON.parse(File.read(file_name))
 end
