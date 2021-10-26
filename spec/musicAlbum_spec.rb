969deff7ce3ff22b5821e4e5cd666f7158fa9608
Caleb Anderson <buyananderson@gmail.com>
2021-10-26 13:09:53 +0200

    Write test When Music is initialise with on_spotity to false

--- spec/musicAlbum_spec.rb
+++ spec/musicAlbum_spec.rb
@@ -9,4 +9,10 @@ describe MusicAlbum do
        end
     end
 
+    context "When Music is initialise with on_spotity to false" do
+       it "Should return an istance of the Class musicAlbum" do
+        new_music_album = MusicAlbum.new("2020-10-10", on_spotify: true)
+        expect(new_music_album).to be_an_instance_of MusicAlbum 
+       end
+    end
 end
