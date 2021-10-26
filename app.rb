12fb027ddf87e1c019f0741a101d4f8fb6350635
Caleb Anderson <buyananderson@gmail.com>
2021-10-26 17:36:36 +0200

    Correct the json file creator

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+
+
 class App
     def initialize
         @genres = []
--- file_manger.rb
+++ file_manger.rb
@@ -1,13 +1,20 @@
 require './main'
 
-def createFile (file_name)
-    FileUtils.touch(file_name)
+def createFile
+    FileUtils.touch("./json/genres.json")
+    FileUtils.touch("./json/labels.json")
+    FileUtils.touch("./json/authors.json")
+    FileUtils.touch("./json/soures.json")
+    FileUtils.touch("./json/books.json")
+    FileUtils.touch("./json/musicAlbum.json")
+    FileUtils.touch("./json/movies.json")
+    FileUtils.touch("./json/games.json")
 end
 
 def save_file(file_name, content)
-    File.write(file_name, JSON.dump(content))
+    File.write("./json/#{file_name}.json", JSON.dump(content))
 end
 
 def read_file(file_name)
-    JSON.parse(File.read(file_name))
+    JSON.parse(File.read("./json/#{file_name}.json"))
 end
